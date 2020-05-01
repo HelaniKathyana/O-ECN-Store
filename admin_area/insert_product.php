c60521a3f0f5b08fab2a0bef2ca6b8660c67d5c5
Helani Waidyarathne <35195335+HelaniKathyana@users.noreply.github.com>
2020-05-01 11:11:06 +0530

    insert product

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -12,8 +12,6 @@ include("includes/db.php");
     <title> Insert Products </title>
     <link rel="stylesheet" href="css/bootstrap-337.min.css">
     <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
-    <script src="js/tinymce/tinymce.min.js"></script>
-    <script>tinymce.init({ selector:'textarea'});</script>
 </head>
 <body>
 
@@ -23,11 +21,11 @@ include("includes/db.php");
 
         <ol class="breadcrumb"><!-- breadcrumb Begin -->
 
-            <li class="active"><!-- breadcrumb Begin -->
+            <li class="active"><!-- active Begin -->
 
                 <i class="fa fa-dashboard"></i> Dashboard / Insert Products
 
-            </li><!-- breadcrumb Finish -->
+            </li><!-- active Finish -->
 
         </ol><!-- breadcrumb Finish -->
 
@@ -41,7 +39,7 @@ include("includes/db.php");
 
         <div class="panel panel-default"><!-- panel panel-default Begin -->
 
-           <div class="panel-heading"><!-- panel panel-default Begin -->
+           <div class="panel-heading"><!-- panel-heading Begin -->
 
                <h3 class="panel-title"><!-- panel-title Begin -->
 
@@ -49,7 +47,7 @@ include("includes/db.php");
 
                </h3><!-- panel-title Finish -->
 
-           </div> <!-- canel panel-default Finish -->
+           </div> <!-- panel-heading Finish -->
 
            <div class="panel-body"><!-- panel-body Begin -->
 
@@ -109,7 +107,7 @@ include("includes/db.php");
 
                       <div class="col-md-6"><!-- col-md-6 Begin -->
 
-                          <select name="product_cat" class="form-control"><!-- form-control Begin -->
+                          <select name="cat" class="form-control"><!-- form-control Begin -->
 
                               <option> Select a Category </option>
 
@@ -235,7 +233,46 @@ include("includes/db.php");
 
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
-
-
+    <script src="js/tinymce/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'textarea'});</script>
 </body>
 </html>
+
+
+<?php
+
+if(isset($_POST['submit'])){
+
+    $product_title = $_POST['product_title'];
+    $product_cat = $_POST['product_cat'];
+    $cat = $_POST['cat'];
+    $product_price = $_POST['product_price'];
+    $product_keywords = $_POST['product_keywords'];
+    $product_desc = $_POST['product_desc'];
+
+    $product_img1 = $_FILES['product_img1']['name'];
+    $product_img2 = $_FILES['product_img2']['name'];
+    $product_img3 = $_FILES['product_img3']['name'];
+
+    $temp_name1 = $_FILES['product_img1']['tmp_name'];
+    $temp_name2 = $_FILES['product_img2']['tmp_name'];
+    $temp_name3 = $_FILES['product_img3']['tmp_name'];
+
+    move_uploaded_file($temp_name1,"product_images/$product_img1");
+    move_uploaded_file($temp_name2,"product_images/$product_img2");
+    move_uploaded_file($temp_name3,"product_images/$product_img3");
+
+    $insert_product = "insert into products (p_cat_id,cat_id,date,product_title,product_img1,product_img2,product_img3,product_price,product_keywords,product_desc) values ('$product_cat','$cat',NOW(),'$product_title','$product_img1','$product_img2','$product_img3','$product_price','$product_keywords','$product_desc')";
+
+    $run_product = mysqli_query($con,$insert_product);
+
+    if($run_product){
+
+        echo "<script>alert('Product has been inserted sucessfully')</script>";
+        echo "<script>window.open('insert_product.php','_self')</script>";
+
+    }
+
+}
+
+?>
