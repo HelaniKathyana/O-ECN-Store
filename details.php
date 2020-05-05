da2e79e180c577da3eb9d4bd1f830ba4f0644ea6
Helani Waidyarathne <35195335+HelaniKathyana@users.noreply.github.com>
2020-05-05 21:33:17 +0530

    Input product requests to the database

--- details.php
+++ details.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<body>
-
 <?php
 
     $active='Cart';
@@ -38,7 +33,7 @@
 
     ?>
 
-   </div><!-- col-md-3 Finish -->
+           </div><!-- col-md-3 Finish -->
 
            <div class="col-md-9"><!-- col-md-9 Begin -->
                <div id="productMain" class="row"><!-- row Begin -->
@@ -81,7 +76,9 @@
                        <div class="box"><!-- box Begin -->
                            <h1 class="text-center"> <?php echo $pro_title; ?> </h1>
 
-                           <form action="index.php?add_cart=<?php echo $pro_id; ?>" class="form-horizontal" method="post"><!-- form-horizontal Begin -->
+                           <?php add_cart(); ?>
+
+                           <form action="details.php?add_cart=<?php echo $product_id; ?>" class="form-horizontal" method="post"><!-- form-horizontal Begin -->
                                <div class="form-group"><!-- form-group Begin -->
                                    <label for="" class="col-md-5 control-label">Products Quantity</label>
 
@@ -103,9 +100,9 @@
 
                                    <div class="col-md-7"><!-- col-md-7 Begin -->
 
-                                       <select name="product_size" class="form-control"><!-- form-control Begin -->
+                                       <select name="product_size" class="form-control" required oninput="setCustomValidity('')" oninvalid="setCustomValidity('Must pick 1 size for the product')"><!-- form-control Begin -->
 
-                                           <option>Select a Size</option>
+                                           <option disabled selected>Select a Size</option>
                                            <option>Small</option>
                                            <option>Medium</option>
                                            <option>Large</option>
@@ -181,7 +178,7 @@
 
                    <?php
 
-                    $get_products = "select * from products order by 1 DESC LIMIT 0,3";
+                    $get_products = "select * from products order by rand() LIMIT 0,3";
 
                     $run_products = mysqli_query($con,$get_products);
 
--- functions/functions.php
+++ functions/functions.php
@@ -2,6 +2,65 @@
 
 $db = mysqli_connect("localhost","root","","oecn_store");
 
+/// begin getRealIpUser functions ///
+
+function getRealIpUser(){
+
+    switch(true){
+
+            case(!empty($_SERVER['HTTP_X_REAL_IP'])) : return $_SERVER['HTTP_X_REAL_IP'];
+            case(!empty($_SERVER['HTTP_CLIENT_IP'])) : return $_SERVER['HTTP_CLIENT_IP'];
+            case(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) : return $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+            default : return $_SERVER['REMOTE_ADDR'];
+
+    }
+
+}
+
+/// begin add_cart functions ///
+
+function add_cart(){
+
+    global $db;
+
+    if(isset($_GET['add_cart'])){
+
+        $ip_add = getRealIpUser();
+
+        $p_id = $_GET['add_cart'];
+
+        $product_qty = $_POST['product_qty'];
+
+        $product_size = $_POST['product_size'];
+
+        $check_product = "select * from cart where ip_add='$ip_add' AND p_id='$p_id'";
+
+        $run_check = mysqli_query($db,$check_product);
+
+        if(mysqli_num_rows($run_check)>0){
+
+            echo "<script>alert('This product has already added in cart')</script>";
+            echo "<script>window.open('details.php?pro_id=$p_id','_self')</script>";
+
+        }else{
+
+            $query = "insert into cart (p_id,ip_add,qty,size) values ('$p_id','$ip_add','$product_qty','$product_size')";
+
+            $run_query = mysqli_query($db,$query);
+
+            echo "<script>window.open('details.php?pro_id=$p_id','_self')</script>";
+
+        }
+
+    }
+
+}
+
+/// finish add_cart functions ///
+
+/// finish getRealIpUser functions ///
+
 /// begin getPro functions ///
 
 function getPro(){
@@ -292,7 +351,7 @@ function getcatpro(){
 
         $cat_desc = $row_cat['cat_desc'];
 
-        $get_cat = "select * from products where cat_id='$cat_id'";
+        $get_cat = "select * from products where cat_id='$cat_id' LIMIT 0,6";
 
         $run_products = mysqli_query($db,$get_cat);
 
