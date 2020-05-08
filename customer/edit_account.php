8081e06d9dd5e8063c6f6928cadd69ce6e2deab2
Helani Waidyarathne <35195335+HelaniKathyana@users.noreply.github.com>
2020-05-08 18:33:36 +0530

    Working With payments edit delete account and change password

--- customer/edit_account.php
+++ customer/edit_account.php
@@ -1,56 +1,141 @@
+<?php
+
+$customer_session = $_SESSION['customer_email'];
+
+$get_customer = "select * from customers where customer_email='$customer_session'";
+
+$run_customer = mysqli_query($con,$get_customer);
+
+$row_customer = mysqli_fetch_array($run_customer);
+
+$customer_id = $row_customer['customer_id'];
+
+$customer_name = $row_customer['customer_name'];
+
+$customer_email = $row_customer['customer_email'];
+
+$customer_country = $row_customer['customer_country'];
+
+$customer_city = $row_customer['customer_city'];
+
+$customer_contact = $row_customer['customer_contact'];
+
+$customer_address = $row_customer['customer_address'];
+
+$customer_image = $row_customer['customer_image'];
+
+?>
+
 <h1 align="center"> Edit Your Account </h1>
 
+<form action="" method="post" enctype="multipart/form-data"><!-- form Begin -->
 
-<form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group"><!-- form-group Begin -->
 
-    <div class="form-group">
-        <label> Customer Name: </label>
+        <label> Costumer Name: </label>
 
-        <input type="text" name="e_name" class="form-control" required>
-    </div>
+        <input type="text" name="c_name" class="form-control" value="<?php echo $customer_name; ?>" required>
 
-    <div class="form-group">
-        <label> Customer Email: </label>
+    </div><!-- form-group Finish -->
 
-        <input type="text" name="e_email" class="form-control" required>
-    </div>
+    <div class="form-group"><!-- form-group Begin -->
 
-    <div class="form-group">
-        <label> Customer Country: </label>
+        <label> Costumer Email: </label>
 
-        <input type="text" name="e_country" class="form-control" required>
-    </div>
+        <input type="text" name="c_email" class="form-control" value="<?php echo $customer_email; ?>" required>
 
-    <div class="form-group">
-        <label> Customer City: </label>
+    </div><!-- form-group Finish -->
 
-        <input type="text" name="e_city" class="form-control" required>
-    </div>
+    <div class="form-group"><!-- form-group Begin -->
 
-    <div class="form-group">
-        <label> Customer Contact: </label>
+        <label> Costumer Country: </label>
 
-        <input type="text" name="e_contact" class="form-control" required>
-    </div>
+        <input type="text" name="c_country" class="form-control" value="<?php echo $customer_country; ?>" required>
 
-    <div class="form-group">
-        <label> Customer Address: </label>
+    </div><!-- form-group Finish -->
 
-        <input type="text" name="e_address" class="form-control" required>
-    </div>
+    <div class="form-group"><!-- form-group Begin -->
 
-    <div class="form-group">
-        <label> Customer Image: </label>
+        <label> Costumer City: </label>
 
-        <input type="file" name="e_image" class="form-control form-height-custom" required>
-        <img src="customer_images/erika.jpg" alt="Customer Image" class="img-responsive">
-    </div>
+        <input type="text" name="c_city" class="form-control" value="<?php echo $customer_city; ?>" required>
 
-    <div class="text-center">
+    </div><!-- form-group Finish -->
 
-        <button class="btn btn-primary" name="update">
+    <div class="form-group"><!-- form-group Begin -->
+
+        <label> Costumer Contact: </label>
+
+        <input type="text" name="c_contact" class="form-control" value="<?php echo $customer_contact; ?>" required>
+
+    </div><!-- form-group Finish -->
+
+    <div class="form-group"><!-- form-group Begin -->
+
+        <label> Costumer Address: </label>
+
+        <input type="text" name="c_address" class="form-control" value="<?php echo $customer_address; ?>" required>
+
+    </div><!-- form-group Finish -->
+
+    <div class="form-group"><!-- form-group Begin -->
+
+        <label> Costumer Image: </label>
+
+        <input type="file" name="c_image" class="form-control form-height-custom">
+
+        <img class="img-responsive" src="customer_images/<?php echo $customer_image; ?>" alt="Costumer Image">
+
+    </div><!-- form-group Finish -->
+
+    <div class="text-center"><!-- text-center Begin -->
+
+        <button name="update" class="btn btn-primary"><!-- btn btn-primary Begin -->
+
             <i class="fa fa-user-md"></i> Update Now
-        </button>
-    </div>
 
-</form>
+        </button><!-- btn btn-primary inish -->
+
+    </div><!-- text-center Finish -->
+
+</form><!-- form Finish -->
+
+<?php
+
+if(isset($_POST['update'])){
+
+    $update_id = $customer_id;
+
+    $c_name = $_POST['c_name'];
+
+    $c_email = $_POST['c_email'];
+
+    $c_country = $_POST['c_country'];
+
+    $c_city = $_POST['c_city'];
+
+    $c_address = $_POST['c_address'];
+
+    $c_contact = $_POST['c_contact'];
+
+    $c_image = $_FILES['c_image']['name'];
+
+    $c_image_tmp = $_FILES['c_image']['tmp_name'];
+
+    move_uploaded_file ($c_image_tmp,"customer_images/$c_image");
+
+    $update_customer = "update customers set customer_name='$c_name',customer_email='$c_email',customer_country='$c_country',customer_city='$c_city',customer_address='$c_address',customer_contact='$c_contact',customer_image='$c_image' where customer_id='$update_id' ";
+
+    $run_customer = mysqli_query($con,$update_customer);
+
+    if($run_customer){
+
+        echo "<script>alert('Your account has been edited, to complete the process, please Relogin')</script>";
+
+        echo "<script>window.open('logout.php','_self')</script>";
+
+    }
+
+}
+
+?>
--- customer/my_orders.php
+++ customer/my_orders.php
@@ -99,7 +99,7 @@
 
                 <td>
 
-                    <a href="confirm.php?order_id='<?php echo $order_id; ?>'" target="_blank" class="btn btn-primary btn-sm"> Confirm Paid </a>
+                    <a href="confirm.php?order_id=<?php echo $order_id; ?>" target="_blank" class="btn btn-primary btn-sm"> Confirm Paid </a>
 
                 </td>
 
